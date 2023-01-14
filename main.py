74c81346e92a7c0ed26070dca6ce71313a638843
Joanna Reszka <j.k.reszka@gmail.com>
2023-01-14 18:49:09 +0100

    Add function retrieving data from Api point

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
+# Plotly - data vizualization library (like Bokeh)
+import plotly.express as px
+from functions import get_data
 
+# Order of code defines order of widgets on the webpage
 st.title("Weather Forecast")
 place = st.text_input("Place: ")
 # creates slider
@@ -9,3 +13,7 @@ days = st.slider("Forecast Days", min_value = 1, max_value =5,
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+d,t = get_data(place, days, option)
+
+figure = px.line()
+st.plotly_chart()
