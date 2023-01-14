06dea33a741d07f9da9a3268d1a55a4814e38e6d
Joanna Reszka <j.k.reszka@gmail.com>
2023-01-14 20:23:28 +0100

    Add images and error handling

--- functions.py
+++ functions.py
@@ -2,18 +2,15 @@ import requests
 # Api Key from weather forecast page
 API_KEY = "d223a5e4db230d49bf08e031ca0a46a0"
  # modified {cityID} with {place} and {Api key} with {API_KEY} and added http://
-def get_data(place, days = None, kind=None):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+def get_data(place, days):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
-    filtered = data["list"]
+    filtered_data = data["list"]
     nr_values = 8 * days
-    filtered = filtered[:nr_values]
-    if kind == "Temperature":
-        filtered = [dict["main"]["temp"] for dict in filtered ]
-    elif kind == "Sky":
-        filtered = [dict["weather"][0]["main"] for dict in filtered]
-    return filtered
+    filtered_data = filtered_data[:nr_values]
+
+    return filtered_data
 
 if __name__=="__main__":
-    get_data(place = "Tokyo", days =3, kind = "Temperature")
+    get_data()
--- main.py
+++ main.py
@@ -5,15 +5,36 @@ from functions import get_data
 
 # Order of code defines order of widgets on the webpage
 st.title("Weather Forecast")
-place = st.text_input("Place: ")
+place = st.text_input("Place: ", value = "London")
 # creates slider
-days = st.slider("Forecast Days", min_value = 1, max_value =5,
-                 help = "Select the number of forecasted days")
+number_of_days = st.slider("Forecast Days", min_value = 1, max_value =5,
+                           help = "Select the number of forecasted days")
 # creates select box
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
-d,t = get_data(place, days, option)
+if number_of_days>1:
+    day_or_days = "days"
+elif number_of_days ==1:
+    day_or_days = "day"
+st.subheader(f"{option} for the next {number_of_days} {day_or_days} in {place}")
 
-figure = px.line()
-st.plotly_chart()
+try:
+    # Get Temperature or sky data
+    filtered = get_data(place, number_of_days)
+
+    if option == "Temperature":
+        # Create a temperature plot
+        temperatures = [dict["main"]["temp"] for dict in filtered]
+        #temperatures = [temp-273.15 for temp in temperatures]
+        dates = [dict["dt_txt"] for dict in filtered]
+        figure = px.line(x=dates, y=temperatures, labels={"x":"Date", "y":"Temperature (C)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered]
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths, width =115)
+except KeyError:
+    st.write("You typed in invalid city, please type again.")
